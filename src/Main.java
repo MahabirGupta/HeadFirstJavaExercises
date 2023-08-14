54d4e97702fbd3eb9cd08a569fc2f99fa4b6e6be
Mahabir <mahabirg81@gmail.com>
2023-08-14 16:11:48 +0800

    added main maethod in Main Clas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("small x");
+            }
+        }
     }
 }
